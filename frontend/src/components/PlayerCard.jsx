bf87be3835af0b94a4dc71512c8b3b73ed2f700e
Jason Wang <jasonwang12@icloud.com>
2023-03-18 23:24:15 -0400

    basic playercard

--- frontend/src/components/PlayerCard.jsx
+++ frontend/src/components/PlayerCard.jsx
@@ -2,4 +2,18 @@ import React from 'react';
 
 export const PlayerCard = (props) => {
     const playerName = props.name;
+    const age = props.age
+    const stats = props.stats
+
+    return (
+        <div>
+            <div>
+                <h1>{playerName}</h1>
+            </div>
+            <div>
+                <p>{age}</p>
+                <p>{stats}</p>
+            </div>
+        </div>
+        )
 }
