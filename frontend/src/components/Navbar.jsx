de41e7660a5da725838ba4452f2815f5dfaf685a
Hengsheng-Liu <lhs7714@bu.edu>
2023-03-30 09:48:46 -0400

    replace navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/Navbar.css";
-
-export const Navbar = () => {
-        return(
-            <div className = "navbar">
-                <ul className = "Pages">
-                    <li>
-                        <Link to="/">Games</Link>
-                    </li>
-                    <li>
-                        <Link to="/teams">Teams</Link>
-                    </li>
-                    <li>
-                        <Link to="/players">Players</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </ul>
-            </div>
-        )
+import { Navbar, Nav } from 'react-bootstrap';
+export const Alt_Navbar = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Navbar.Brand href="/">My App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/">Games</Nav.Link>
+                    <Nav.Link as={Link} to="/teams">Teams</Nav.Link>
+                    <Nav.Link as={Link} to="/players">Players</Nav.Link>
+                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
 }
+
