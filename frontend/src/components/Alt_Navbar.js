42d1605112073547e6dec795794c7789b5fd65b8
Hengsheng-Liu <lhs7714@bu.edu>
2023-03-30 09:13:19 -0400

    debugged navbar

--- frontend/src/components/Alt_Navbar.js
+++ frontend/src/components/Alt_Navbar.js
@@ -4,14 +4,17 @@ import "../styles/Navbar.css";
 import { Navbar, Nav} from 'react-bootstrap';
 const Alt_Navbar = () => {
     return (
-        <Navbar bg="light" variant="light">
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Navbar.Brand href="/">My App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                     <Nav.Link as={Link} to="/">Games</Nav.Link>
                     <Nav.Link as={Link} to="/teams">Teams</Nav.Link>
                     <Nav.Link as={Link} to="/players">Players</Nav.Link>
                     <Nav.Link as={Link} to="/login">Login</Nav.Link>
                 </Nav>
+            </Navbar.Collapse>
         </Navbar>
 );
 }
