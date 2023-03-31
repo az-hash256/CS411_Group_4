4c79fa91df8dd4abdc216c4e3f15fc03310a1674
Jason Wang <jasonwang12@icloud.com>
2023-03-30 21:28:44 -0400

    routes folder

---
+++ backend/routes/teams.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/', async (req, res) => {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://api-nba-v1.p.rapidapi.com/teams',
+      headers: {
+        'X-RapidAPI-Key': process.env.RAPID_KEY,
+        'X-RapidAPI-Host': process.env.RAPID_HOST
+      }
+    };
+    const response = await axios.request(options);
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+module.exports = router;
