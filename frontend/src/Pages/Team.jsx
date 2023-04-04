bb8d220e2422cc8a7948011f7c998f034f5687b3
Hengsheng-Liu <lhs7714@bu.edu>
2023-04-04 10:53:37 -0400

    tryn to finish single team

--- frontend/src/Pages/Team.jsx
+++ frontend/src/Pages/Team.jsx
@@ -1,11 +1,14 @@
 import { React, useState, useEffect } from 'react';
-import { TeamCard } from './../components/SingleTeam';
+import { SingleTeam } from './../components/SingleTeam';
+import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import axios from "axios";
-export const Team = (props) => {
-    console.log("hello");
+export const Team = () => {
+    const {teamName} = useParams(); 
+    const { state: { teamLogo } } = useLocation();
     return (
         <div>
-            <h1>Hello</h1>
+            <SingleTeam teamName={teamName} teamLogo={teamLogo} />
         </div>
     )
 }
