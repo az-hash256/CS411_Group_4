5e7ba29e4cda1bcc452ac458f3ccc7bbb90b3c08
Jason Wang <jasonwang12@icloud.com>
2023-04-04 14:43:03 -0400

    trying to use a db

--- backend/routes/loginDB.js
+++ backend/routes/loginDB.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const userId = require('./schemas/userSchema');
+const userId = require('../schemas/userSchema');
+const jwt = require('jsonwebtoken');
 
-router.post('/', (req, res) => {
+router.post('/users', (req, res) => {
     const userInfo = new userId({
-        googlecred: req.body.credential,
+        googlecred: jwt.decode(req.body.credential),
         favoriteTeam: ""
     })
+
+    userInfo.save((err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error saving user to database');
+        } else {
+          console.log('User saved to database');
+          res.status(200).send('User saved to database');
+        }
+      });
 })
+
+module.exports = router;
